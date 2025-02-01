a0c458c51eee2dcb3814188b9acf6827bc700a59
Jared Lee <jaredjle@usc.edu>
2025-02-01 00:50:56 +0000

    all done with part q

--- split.cpp
+++ split.cpp
@@ -16,8 +16,45 @@ the function below should be the only one in this file.
 
 void split(Node*& in, Node*& odds, Node*& evens)
 {
-  /* Add code here */
-// WRITE YOUR CODE HERE
+  // base case if input list is empty, return
+  if(in == nullptr){
+    return;
+  }
+
+  // check if current node's value is odd
+  if(in->value % 2 != 0){
+    // if odds list is empty initialize with current node
+    if(odds == nullptr){
+      odds = in;
+      in = in->next; // move input pointer to next node
+      odds->next = nullptr;
+      split(in, odds, evens); // recursively process the rest of the list
+    }
+    // if odds list exists, append current node to it
+    else {
+      odds->next = in; // connect current node to odds list
+      in = in->next;  // move input pointer to next node
+      odds->next->next = nullptr;
+      split(in, odds->next, evens); // recursively process the rest of the list
+    }
+  }
+  // handles even values in the current node
+  else {
+    // if evens list is empty initialize with current node
+    if(evens == nullptr){
+      evens = in;
+      in = in->next; // move input pointer to next node
+      evens->next = nullptr;
+      split(in, odds, evens); // recursively process the rest of the list
+    }
+    // if odds list exists, append current node to it
+    else {
+      evens->next = in; // connect current node to evens list
+      in = in->next; // move input pointer to next node
+      evens->next->next = nullptr;
+      split(in, odds, evens->next); // recursively process the rest of the list
+    }
+  }
 }
 
 /* If you needed a helper function, write it here */
